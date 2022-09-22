4f5f763d1da1550288af055b3f46e55b52834454
ficadjole <velemirfilip@gmail.com>
2022-09-22 20:06:22 +0200

    navbar collapse

--- java.js
+++ java.js
@@ -1,16 +1,25 @@
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover({
-         placement: 'right',
-         trigger: 'click', 
-         html: true,
-         content: function () {
-               return '<img class="img-fluid" src="'+$(this).data('img') + '" />';
-         },       
-   }) 
+$(document).ready(function () {
+      $('[data-toggle="popover"]').popover({
+            placement: 'right',
+            trigger: 'click',
+            html: true,
+            content: function () {
+                  return '<img class="img-fluid" src="' + $(this).data('img') + '" />';
+            },
+      })
 });
 
-function black(){
-      filip.classList.add('bg-dark');
-}
+/* $(document).ready(function () {
+      if (window.innerWidth < 480) {
+            $('.btn').addClass('').removeClass('btn');
+            $('.color-text').addClass('nav-mobile').removeClass('color-text');
+      }
+});
 
-
+$(window).resize(function () {
+      if (window.innerWidth < 1340) {
+            $('.btn').addClass('').removeClass('btn');
+      } else {
+            $('').addClass('btn').removeClass('');
+      }
+});  */
