dd6713be425f312732569cf4ab57af1f9af0c2b1
ficadjole <velemirfilip@gmail.com>
2022-09-27 16:43:47 +0200

    katalog

--- java.js
+++ java.js
@@ -8,18 +8,3 @@ $(document).ready(function () {
             },
       })
 });
-
-/* $(document).ready(function () {
-      if (window.innerWidth < 480) {
-            $('.btn').addClass('').removeClass('btn');
-            $('.color-text').addClass('nav-mobile').removeClass('color-text');
-      }
-});
-
-$(window).resize(function () {
-      if (window.innerWidth < 1340) {
-            $('.btn').addClass('').removeClass('btn');
-      } else {
-            $('').addClass('btn').removeClass('');
-      }
-});  */
