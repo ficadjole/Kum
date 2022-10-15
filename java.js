c014758c45828f5b681f19dab3831b076fa1a387
ficadjole <velemirfilip@gmail.com>
2022-10-15 13:18:47 +0200

    popovers2

--- java.js
+++ java.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
       $('[data-toggle="popover"]').popover({
-            placement: 'right',
+            placement: 'left',
             trigger: 'click',
             html: true,
             content: function () {
-                  return '<img class="img-fluid" src="' + $(this).data('img') + '" />';
+                  return '<img class="img-fluid"  src="'+$(this).data('img') + '" />';
             },
       })
 });
