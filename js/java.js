3251325475b0be73fde8308098a15e2fe447c157
ficadjole <velemirfilip@gmail.com>
2022-10-31 15:11:12 +0100

    final

--- js/java.js
+++ js/java.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
       $('[data-toggle="popover"]').popover({
-            placement: 'left',
-            trigger: 'click',
+            placement: "left",
+            trigger: "focus",
             html: true,
             content: function () {
-                  return '<img class="img-fluid"  src="'+$(this).data('img') + '" />';
+                  return '<img class="img-fluid"  src="' + $(this).data("img") + '" />';
             },
-      })
+      });
+});
+$(document).ready(function () {
+      const $nav = $("#navbarSupportedContent");
+      $nav.find(".nav-link").on("click", () => {
+            setTimeout(() => {
+                  $nav.collapse("hide");
+            }, 300);
+      });
 });
